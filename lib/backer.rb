e449f6d34b7c2f1c2d21cdf6865625cdb715c57e
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-04-26 19:32:36 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,12 @@
 class Backer
   attr_accessor :backed_projects, :name
-
   def initialize(name)
     @name = name
-    @backed_projects = []
+    @backed_projects = [] 
   end
-
+  
   def back_project(project)
-    @backed_projects << project
-    project.add_backer(self) unless project.backers.include?(self)
+   @backed_projects << project 
+   project.backers << self 
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,13 @@
 class Project
-
   attr_accessor :backers, :title
-
   def initialize(title)
-    @title = title
+    @title = title 
     @backers = []
   end
 
   def add_backer(backer)
-    @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    @backers << backer 
+    backer.backed_projects << self 
   end
 
 end
